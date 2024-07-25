51e512dea0e049c55f618073a2c054ff46a2c666
Anastasiia Bulba <fedinanastya@gmail.com>
2024-07-25 11:02:16 +0300

    tasks1.1

--- js/my-task-1.js
+++ js/my-task-1.js
@@ -1,1 +1,48 @@
 'use strict';
+
+// Перевірка числа мінусове чи ні
+let numbers = 15;
+
+if (numbers < 0) {
+  console.log('Negative numbers');
+} else {
+  console.log('Positive numbers');
+}
+// ------------------------------
+
+// Довжина рядка
+const lineNew = 'Welcome to Ukraine!';
+console.log(lineNew.length);
+// ------------------------------
+
+// Останній символ
+let symbolNew = 'Welcome to Ukraine!';
+let endSymbol = symbolNew.length - 1;
+console.log(symbolNew[endSymbol]);
+// ------------------------------
+
+// Парне не парне
+let nam = 38;
+if (nam % 2 === 0) {
+  console.log('Парне число');
+} else {
+  console.log('Не парне число');
+}
+// ------------------------------
+
+// Перевірка перших букв
+function wordsAudit(words1, words2) {
+  let words1Lower = words1.toLowerCase();
+  let words2Lower = words2.toLowerCase();
+
+  if (words1Lower[0] === words2Lower[0]) {
+    return 'Співпадіння';
+  } else {
+    return 'Не співпадає';
+  }
+}
+console.log(wordsAudit('Audit', 'autumn'));
+console.log(wordsAudit('Audit', 'bautumn'));
+// ------------------------------
+
+// Остання буква зі слова
